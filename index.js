1070696b860537c0607543ed26c8f36bc1a49a40
BBBmau <65101411+BBBmau@users.noreply.github.com>
2020-05-21 23:36:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NzEzMjU2NjU4NjYyNTg4NDE4.XsdhlA.SsP8lrvz5NPkOp0hE_9JXb_qRgE';
+const token = 'TOKEN_#';
 
 bot.on('ready', () =>{
     console.log('This bot is online!')
