6defd19027a02b1494467746158a63046b78a44c
BBBmau <65101411+BBBmau@users.noreply.github.com>
2020-05-22 13:57:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const { Client, MessageAttachment } = require('discord.js');
 
-const token = 'TOKEN_#';
+const token = 'NzEzMjU2NjU4NjYyNTg4NDE4.XseMQw.rB15ERQirpNoVZhPLP9fgQBHoWU';
 
 bot.on('ready', () =>{
     console.log('This bot is online!')
 })
 
+bot.on('message', msg=> {
+    if(msg.content === "$Hello"){
+        msg.channel.send(`${msg.author}, Are you a Big Baller?`);
+    }
+}
+)
+
+bot.on('message', msg=> {
+    if(msg.content === "$Help"){
+    const BBB = new MessageAttachment('https://i.imgur.com/aqzPoH8.jpg')
+
+        msg.channel.send(`Big Ballers don't need HELP` , BBB );
+    }
+}
+)
+
+bot.on('message', msg => {
+    // If the message is "what is my avatar"
+    if (msg.content === '$Avatar') {
+      // Send the user's avatar URL
+      msg.reply(msg.author.displayAvatarURL());
+    }
+  });
+
+  bot.on('message', msg => {
+    // If the message is '!rip'
+    if (msg.content === '$Drew') {
+      // Create the attachment using MessageAttachment
+      const attachment =  new MessageAttachment('https://i.imgur.com/UKQPMt2.png');
+      // Send the attachment in the message channel
+      msg.channel.send(`${msg.author}, Yikes!`, attachment);
+    }
+  });
+
+  bot.on('message', msg=>{
+    if(msg.content === "$N"){
+        msg.reply('NIGGER.');
+    }
+}
+)
+
 bot.on('message', msg=>{
     if(msg.content === "$BBB"){
         msg.reply('BIG BALLER BRAND');
     }
-})
+}
+)
 
 bot.login(token);
